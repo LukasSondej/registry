44b29c2a875faba0bfddd2f5bb0295e938c6cf45
DESKTOP-IG0P9FQ\lukas <sondejlukasz0@gmail.com>
2023-03-13 22:25:02 +0100

    takie cos

---
+++ src/main/java/org/example/ChoseClass.java
@@ -0,0 +1,102 @@
+package org.example;
+import com.mysql.cj.Query;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ChoseClass extends JFrame
+{
+
+    JFrame frame = new JFrame();
+    JButton clas1 = new JButton();
+    JButton clas2 = new JButton();
+    JButton clas3 = new JButton();
+
+    JButton setting = new JButton();
+    JButton button = new JButton();
+    JPanel panel = new JPanel();
+
+    public Connection getConnect(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/diary3", "root", "Rafalek");
+        }catch(SQLException ex) {
+            Logger.getLogger(Query.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        return  con;
+    }
+    ChoseClass(){
+        ArrayList<String> lista = new ArrayList<>();
+        try {
+            Connection con = getConnect();
+            Statement stmt = con.createStatement();
+            String querry = "select klasa from klasy";
+            ResultSet rs = stmt.executeQuery(querry);
+
+            while (rs.next()){
+                lista.add(rs.getString("klasa"));
+            }
+
+
+        }
+        catch (Exception e){
+
+        }
+
+        for (int i = 0; i < lista.size(); i++) {
+           String s = lista.get(i);
+            System.out.println(s);
+        }
+int i = 0;
+        while (i<lista.size())
+        {
+
+            button =  new JButton(lista.get(i));
+            button.setBounds(200,i*100,100,100);
+            panel.add(button);
+            i++;
+
+        }
+
+
+
+
+
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        frame.setSize(600,600);
+        panel.setLayout(null);
+        panel.add(clas1);
+        panel.add(clas2);
+        panel.add(clas3);
+        panel.add(setting);
+        frame.add(panel);
+        panel.setVisible(true);
+        frame.setVisible(true);
+
+
+
+        clas1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent w){
+
+
+
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       Login1 login1 = new Login1();
+      ChoseClass choseClass = new ChoseClass();
     }
 }
